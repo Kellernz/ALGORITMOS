04ee2057a2fc99d3a9de67f4f28b548cd14ca531
Maçã com Banana <162522557+Kellernz@users.noreply.github.com>
2024-03-08 21:59:22 -0300

    Add files via upload

---
+++ keller.js
@@ -0,0 +1,11 @@
+const number1 = 210
+const number2 = 210
+const gui = "gui"
+const lher = "lher"
+const me = "me"
+console.log(number1+number2)
+console.log(gui+lher+me)
+const jogo = "Fallout"
+const jogosPrincipais = 4
+console.log(typeof jogo)
+console.log(typeof jogosPrincipais)
